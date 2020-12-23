724409d2463b4dd968562b98ca88bdb000f26dcd
Bart73-v <bart.veldman@live.nl>
2020-12-23 10:58:40 +0100

    added papers

--- mainBart.py
+++ mainBart.py
@@ -5,6 +5,14 @@ import pandas as pd
 from sklearn import tree
 import re
 
+
+# Mean-center & normalize data
+# Alter nominal data
+# Hyper parameters?
+# 
+
+#-----------------------------------------------------------
+
 def encode(df):
     encoder = LabelEncoder()
     for column in df.columns:
@@ -15,22 +23,39 @@ def encode(df):
                     break
     return df
 
-df = pd.read_csv('.\student-mat.csv', sep=';')
-df = encode(df)
 
-data = np.array(df)
-attributes = np.array(df.columns)
-y = data[:,-1]
-for ind, grade in enumerate(y):
-    if grade >= 10:
-        y[ind] = 1
-    else: y[ind] = 0
+def createYBin(y):
+    for ind, val in enumerate(y):
+        if val >= 10:
+            y[ind] = 1
+        else: y[ind] = 0
+    return y
 
 
-data = np.delete(data, -1, 1)
-data = np.delete(data, -1, 1)
-data = np.delete(data, -1, 1)
+def createYClas(y):
+    for ind, val in enumerate(y):
+        if val >= 16:
+            y[ind] = 4
+        elif val >= 12:
+            y[ind] = 3
+        elif val >= 8:
+            y[ind] = 2
+        elif val >= 4:
+            y[ind] = 1
+        else: y[ind] = 0
+    return y
 
+
+def getData():
+    df = pd.read_csv('.\student-mat.csv', sep=';')
+    df = encode(df)
+    data = np.array(df)
+    attributes = np.array(df.columns)
+    y = data[:,-1]
+    data = np.delete(data, -1, 1)
+    data = np.delete(data, -1, 1)
+    data = np.delete(data, -1, 1)
+    return data, y, attributes
 
 
 def get_clf_tree(max_depth):
@@ -39,9 +64,18 @@ def get_clf_tree(max_depth):
     return clf
 
 
-fig = plt.figure(figsize=[20,10])
-clf = get_clf_tree(999)
-clf = clf.fit(data,y)
-fig = tree.plot_tree(clf, feature_names=attributes, fontsize=7)
-plt.show()
-print("Figure 1: Decision tree for the Wine dataset with a sample split of 100")
+def print_clf_tree(X, y, attributes):
+    fig = plt.figure(figsize=[20,10])
+    clf = get_clf_tree(999)
+    clf = clf.fit(X,y)
+    fig = tree.plot_tree(clf, feature_names=attributes, fontsize=7)
+    plt.show()
+    print("Figure 1: ")
+
+#-----------------------------------------------------------
+
+X, y, attributes = getData()
+yBin = createYBin(y)
+yClas = createYClas(y)
+
+print(X)
