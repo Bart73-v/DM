8f5371beca37e3b711b711593789e2ba5ca52303
Anton Ligterink <31920375+Anton489@users.noreply.github.com>
2020-12-24 13:16:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,85 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 import pandas as pd
 import re
-from sklearn import tree
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
 
+# def ReadFile(path):
+#     df = pd.read_csv(path, sep=';')
+#     df, indexDict = Encode(df)
+#     X = np.array(df.iloc[:,:-3])
+#     y = df.iloc[:,-1]
+#     y = np.array([0 if x < 10 else 1 for x in y])
+#     return X, y
 
+# def Encode(df):
+#     indexDict = dict()
+#     encoder = LabelEncoder()
+#     for column in df.columns:
+#         for unique in df[column].unique():
+#             if isinstance(unique, str):
+#                 if not re.match('^[0-9]*$', unique):
+#                     uniques = df[column].unique()
+#                     df[column] = encoder.fit_transform(df[column])
+#                     encoding = df[column].unique()
+
+#                     indexDict[column] = dict()
+#                     for i in range(len(uniques)):
+#                         indexDict[column][encoding[i]] = uniques[i]
+#                     break
+#     return df, indexDict
+
+def ReadFile(path):
+    df = pd.read_csv(path, sep=';')
+    y = df.iloc[:,-1]
+    y = np.array([0 if x < 10 else 1 for x in y])
+
+    X = np.array(Encode(df.iloc[:,:-3]))
+    print(X.shape)
+    return X, y
 
 def Encode(df):
-    encoder = LabelEncoder()
-    for column in df.columns:
-        for unique in df[column].unique():
-            if isinstance(unique, str):
-                if not re.match('^[0-9]*$', unique):
-                    df[column] = encoder.fit_transform(df[column])
-                    break
-    return df
+    return OneHotEncoder().fit_transform(df).toarray()
+    
 
-df = pd.read_csv(R'C:\Users\anton\Documents\Data science\Data_mining\Project\data\student-mat.csv', sep=';')
-df = Encode(df)
-print(df)
+def Regression(path):
+    X, y = ReadFile(path)
+    kf = KFold(n_splits=5, random_state=10, shuffle=True)
+    model = LogisticRegression(solver='liblinear')
+    result = cross_val_score(model, X, y, cv = kf)
+    print("Avg regression accuracy: {}".format(result.mean()))
 
+def SVM(path):
+    X, y = ReadFile(path)
+    # model = SVC(kernel = 'linear')
+    # # model = SVC(kernel = 'linear',gamma = 'scale', shrinking = False)
+    # model.fit(X, y)
+    kf = KFold(n_splits=5, random_state=10, shuffle=True)
+    result = []
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]    
 
+        # scaler = StandardScaler()
+        # scaler.fit(X_train)
+        # X_train = scaler.transform(X_train)
+        # X_test = scaler.transform(X_test)
 
-def get_clf_tree(max_depth):
-    clf = tree.DecisionTreeClassifier(max_depth=max_depth)
-    clf.min_samples_split = 100
-    return clf
+        # kf = KFold(n_splits=5, random_state=10, shuffle=True)
+        model = SVC(kernel = 'linear',gamma = 'scale', shrinking = False)
+        model.fit(X_train, y_train)
+        # result = cross_val_score(model, X, y, cv = kf)
+        result.append(model.score(X_test, y_test))
 
-fig = plt.figure(figsize=[100,100])
-clf = tree.DecisionTreeClassifier(max_depth=999)
-clf.min_samples_split = 100
-clf = clf.fit(X,y)
-fig = tree.plot_tree(clf, feature_names=attribute_names, fontsize=40)
-plt.show()
+    print("Avg SVM accuracy: {}".format(sum(result)/len(result)))
+
+path = R'C:\Users\anton\Documents\Data science\Data mining\Project\data\student-mat.csv'
+SVM(path)
+# Regression(path)
 
